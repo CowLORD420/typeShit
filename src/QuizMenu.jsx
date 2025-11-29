23df51ff9130bb0d51606d269c8231f2466b9bb3
Jenaru Matei <mateijenaru123@gmail.com>
2025-11-29 17:01:25 +0200

    Add/update project files

--- src/QuizMenu.jsx
+++ src/QuizMenu.jsx
@@ -3,15 +3,18 @@ import Buttons from "./Buttons";
 function QuizMenu({ onSelectQuiz }) {
   return (
     <div className="quiz-menu">
-      <div class="bg-shape bg1"></div>
-      <div class="bg-shape bg2"></div>
-      <div class="bg-shape bg3"></div>
-      <div className="buttons single-quiz">
-        <Buttons
-          choice="Afla ce surub esti!"
-          onClick={() => onSelectQuiz("start")}
-          className="irresistible"
-        />
+      <div class="bg-shape bg1">
+        <div class="bg-shape bg2">
+          <div class="bg-shape bg3">
+            <div className="buttons single-quiz">
+              <Buttons
+                choice="Afla ce surub esti!"
+                onClick={() => onSelectQuiz("start")}
+                className="irresistible"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

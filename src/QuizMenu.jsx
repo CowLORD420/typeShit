dc12429b73f6ece00cf3d6bf49986dcbad74b3d7
Jenaru Matei <mateijenaru123@gmail.com>
2025-11-29 16:30:24 +0200

    Add/update project files

--- src/QuizMenu.jsx
+++ src/QuizMenu.jsx
@@ -3,7 +3,6 @@ import Buttons from "./Buttons";
 function QuizMenu({ onSelectQuiz }) {
   return (
     <div className="quiz-menu">
-      <h1 className="question">Isdjfkgusagfdyusgfuyfgsdihf iudsfgsdgf8y</h1>
       <div className="buttons single-quiz">
         <Buttons
           choice="Afla ce surub esti!"

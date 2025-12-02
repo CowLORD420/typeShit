581d888d80fa7f9490e5548422f64e8cc61f7434
Jenaru Matei <mateijenaru123@gmail.com>
2025-12-02 20:05:58 +0200

    Add/update project files

--- src/QuizMenu.jsx
+++ src/QuizMenu.jsx
@@ -3,6 +3,8 @@ import Buttons from "./Buttons";
 function QuizMenu({ onSelectQuiz }) {
   return (
     <div className="quiz-menu">
+      <h1 className="EpsilonText">Epsilon #21044</h1>
+      <img src="/Epsilon.png" className="Robot"></img>
       <div className="buttons single-quiz">
         <Buttons
           choice="Afla ce surub esti!"
